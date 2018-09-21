5b2b3f5a1422792045792f340e48c679febbcad0
Kairon Velozo <kairon333@gmail.com>
2018-09-21 16:59:02 -0300

    Barra vermelha por novo teste para a mesma unidade implementada.

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -6,29 +6,53 @@ public class Aplicacao {
 	private float rendimento_bruto;
 	private float rendimento_liquido;
 	private float imposto_renda;
+	private float aliquota;
 	
 	public Aplicacao(float periodo, float taxa_juros, float capital_inicial) {
 		super();
 		this.periodo = periodo;
-		this.taxa_juros = taxa_juros;
+		this.taxa_juros = taxa_juros / 100f;
 		this.capital_inicial = capital_inicial;
 		this.calcularAplicacao();
 	}	
 	
 	private void calcularAplicacao() {
+		this.calcularAliquota();
+		
+		this.calcularRendimentoBruto();
+	}
+	
+	private float calcularPeriodoRelativo() {
+		return this.periodo/365f;
+	}
+
+	private void calcularRendimentoBruto() {
+		this.rendimento_bruto = 0f;
+	}
+	
+	private void calcularRendimentoLiquido() {
+		this.rendimento_liquido = 0f;
+		
+	}
+	
+	private void calcularImpostoRenda() {
+		this.imposto_renda = 0f;
+	}
+	
+	private void calcularAliquota() {
 		
 	}
 	
 	public float getRendimentoBruto() {
-		return 13.97f;
+		return rendimento_bruto;
 	}
 	
 	public float getImpostoRenda() {
-		return 3.14f;
+		return imposto_renda;
 	}
 	
 	public float getRendimentoLiquido() { 
-		return 1.0829f;
+		return rendimento_liquido;
 	}
 	
 	
