312bc9304508b30e7b69237631394860f7a8a8aa
Kairon Velozo <kairon333@gmail.com>
2018-09-21 16:41:35 -0300

    barra verde por falsificação.

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -3,12 +3,33 @@ public class Aplicacao {
 	private float periodo;
 	private float taxa_juros;
 	private float capital_inicial;
+	private float rendimento_bruto;
+	private float rendimento_liquido;
+	private float imposto_renda;
 	
 	public Aplicacao(float periodo, float taxa_juros, float capital_inicial) {
 		super();
 		this.periodo = periodo;
 		this.taxa_juros = taxa_juros;
 		this.capital_inicial = capital_inicial;
+		this.calcularAplicacao();
 	}	
+	
+	private void calcularAplicacao() {
+		
+	}
+	
+	public float getRendimentoBruto() {
+		return 13.97f;
+	}
+	
+	public float getImpostoRenda() {
+		return 3.14f;
+	}
+	
+	public float getRendimentoLiquido() { 
+		return 1.0829f;
+	}
+	
 	
 }
